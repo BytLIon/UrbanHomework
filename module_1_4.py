bc93e3aa75b19592ab4e4862fbdee9d3d26e487a
Lion <lbita2305@gmail.com>
2024-10-10 10:45:04 +0300

    Add files via upload
    1."Организация программ и методы строк"

---
+++ module_1_4.py
@@ -0,0 +1,13 @@
+def program_8():
+    my_string = input()
+
+    print(my_string.upper())
+    print(my_string.lower())
+    print(my_string.replace(' ', ''))
+    print(my_string[0])
+    print(my_string[-1])
+
+
+
+if __name__ == '__main__':
+    program_8()
