965e773c2f50ea5576f8e8be2c6a2b120a0f2b00
Lion <lbita2305@gmail.com>
2024-10-18 14:33:46 +0300

    Add files via upload
    1."Практика 1 и 2"

---
+++ sortfunc.py
@@ -0,0 +1,42 @@
+def bubble_sort(ls):
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(ls) - 1):
+            if ls[i] > ls[i+1]:
+                ls[i], ls[i+1] = ls[i+1], ls[i]
+                swapped = True
+
+    return ls
+
+
+def selection_sort(ls):
+    for i in range(len(ls)):
+        lowest = i
+        for j in range(i + 1, len(ls)):
+            if ls[j] < ls[lowest]:
+                lowest = j
+        ls[i], ls[lowest] = ls[lowest], ls[i]
+
+    return ls
+
+
+
+def insertion_sort(ls):
+    for i in range(1, len(ls)):
+        key = ls[i]
+        j = i - 1
+        while ls[j] > key and j >= 0:
+            ls[j + 1] = ls[j]
+            j -= 1
+        ls[j + 1] = key
+
+    return ls
+
+
+
+if __name__ == '__main__':
+    nums = [5, 6, 2, 1, 3, 4]
+    buble_sort(nums)
+    selection_sort(nums)
+    insertion_sort(nums)
