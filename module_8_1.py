83df988adfe642be6a791d2de7f29739921c1efb
Lion <lbita2305@gmail.com>
2024-12-24 09:17:12 +0300

    1."Try и Except"

---
+++ module_8_1.py
@@ -0,0 +1,19 @@
+def add_everything_up(a, b):
+    try:
+        return a + b
+    except:
+        return f"{a}{b}"
+
+if __name__ == '__main__':
+    print(add_everything_up(123.456, 'строка'))
+    print(add_everything_up('яблоко', 4215))
+    print(add_everything_up(123.456, 7))
+
+
+
+
+
+
+
+
+
